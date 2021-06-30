9df4b6f94f9d7fb05497642ecf53f7e1c70999be
Levon <73011326+Levon1999@users.noreply.github.com>
2021-06-30 19:00:55 +0400

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+int ConsoleOutput(int num) {
+	std::cout << " Multiply and minus for this number " << std::endl;
+	std::cout << " Doing minus by 5 = " << num - 5 << std::endl;
+	std::cout << " Doing multiply by 5 = " << num * 5 << std::endl;
+	return 0;
+}
+int main() {
+	std::cout << "Please enter a number " << std::endl;
+	int num = 0;
+	std::cin >> num;
+	ConsoleOutput(num);
+}
