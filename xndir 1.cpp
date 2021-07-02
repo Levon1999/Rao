c10c6055d56f8bc60e6be1f4a9fc83a060304176
Levon <73011326+Levon1999@users.noreply.github.com>
2021-07-02 18:18:55 +0400

    Add files via upload

---
+++ xndir 1.cpp
@@ -0,0 +1,11 @@
+﻿#include<iostream>
+int main() {
+	int arr[4] = { 14,22,3,18 };
+	int max = arr[0];
+	for (int i = 0; i < 4; ++i) {
+		if (arr[i] > max) {
+			max = arr[i];
+			std::cout << i;
+		}
+	}
+}
