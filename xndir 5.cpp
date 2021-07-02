e1844d74bac5b6cb0bb319e9e8dc7896b4878bc5
Levon <73011326+Levon1999@users.noreply.github.com>
2021-07-02 18:37:41 +0400

    Add files via upload

---
+++ xndir 5.cpp
@@ -0,0 +1,10 @@
+﻿#include<iostream>
+int main() {
+	int ar[5] = { 11,34,3,7,86 };
+	int arr[5] = { 37,2,99,12,4 };
+	int mul = 0;
+	for (int i = 0; i <= 4; ++i) {
+		mul = ar[i] * arr[i];
+		std::cout << mul << std::endl;
+	}
+}
