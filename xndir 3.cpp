b657d3f5c2f4c1bcc2bdd8d69fcc45ccb0157496
Levon <73011326+Levon1999@users.noreply.github.com>
2021-07-02 18:27:38 +0400

    Add files via upload

---
+++ xndir 3.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+int main() {
+	int arr[5] = { 12,31,48,65,4 };
+	int min = arr[0];
+	int max = arr[0];
+	for (int i = 0; i < 5; ++i) {
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+	}
+	for (int ix = 0; ix < 5; ++ix) {
+		if(arr[ix]<min){
+			min = arr[ix];
+		}
+	}
+	std::cout << max + min;
+}
