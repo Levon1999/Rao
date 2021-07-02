4bd193cdab8bc88ed81c25942b11f159c8b2cd9b
Levon <73011326+Levon1999@users.noreply.github.com>
2021-07-02 18:42:44 +0400

    Add files via upload

---
+++ xndir 6.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+int sumArrays() {
+	int arr[3] = { 10,50,90 };
+	int azr[3] = { 21,30,6 };
+	int sum = 0;
+	for (int i = 0; i < 3; ++i) {
+		sum = arr[i] + azr[i];
+		std::cout << sum << std::endl;
+	}
+	return 1;
+}
+int main() {
+	sumArrays();
+}
