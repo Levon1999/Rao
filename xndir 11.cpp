5eab9510787ac1e2cb95c95e5657e579e120e920
Levon <73011326+Levon1999@users.noreply.github.com>
2021-07-06 15:19:46 +0400

    Add files via upload

---
+++ xndir 11.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+int main() {
+	const int size = 5;
+	int arr[size] = { 0 };
+	int count = 0;
+	for (int i = 0; i < 5; ++i) {
+		std::cin >> arr[i];
+		if (arr[i] % 2 == 1) {
+			++count;
+		}
+	}
+	std::cout << count;
+}
