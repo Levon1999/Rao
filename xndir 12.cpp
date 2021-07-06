57f2d4f9fa57070e4e24cfafb1bf96915af214b1
Levon <73011326+Levon1999@users.noreply.github.com>
2021-07-06 15:23:35 +0400

    Add files via upload

---
+++ xndir 12.cpp
@@ -0,0 +1,13 @@
+﻿#include<iostream>
+int main() {
+	const int size = 7;
+	int arr[size] = { 0 };
+	int temp = 0;
+	for (int ix = 0; ix <= 6; ++ix) {
+		std::cin >> arr[ix];
+		if (arr[ix] % 2 == 0) {
+			++temp;
+		}
+	}
+	std::cout << temp;
+}
