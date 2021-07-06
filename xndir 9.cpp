3fdb968749ad9dc64cde07a9a08d51b275cac321
Levon <73011326+Levon1999@users.noreply.github.com>
2021-07-06 14:22:15 +0400

    Add files via upload

---
+++ xndir 9.cpp
@@ -0,0 +1,7 @@
+﻿#include <iostream>
+int main() {
+	int matrix[3][3] = { {1,2,3},{4,5,6},{7,8,9} };
+	for (int i = 0; i < 3; ++i) {
+		std::cout << matrix[i][i] + matrix[i][2-i] << std::endl;
+	}
+}
