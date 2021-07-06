0df1d117cc64540024603fc57c9ecd54e0873094
Levon <73011326+Levon1999@users.noreply.github.com>
2021-07-06 15:14:08 +0400

    Add files via upload

---
+++ xndir 10.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+int main() {
+	int matrix[3][3] = { {1,2,3},{4,5,6},{7,8,9} };
+	int col = 0;
+	int row = 0;
+	for (int i = 0; i < 3; ++i) {
+		col= matrix[i][i];
+		row= matrix[i][2 - i];
+		std::swap(col, row);
+		std::cout << col << " ";
+		std::cout << row << std::endl;
+	}
+}
