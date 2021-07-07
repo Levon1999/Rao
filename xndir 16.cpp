9f7b2c5e3ccf2d2ba44a1addc8e245129388ec22
Levon <73011326+Levon1999@users.noreply.github.com>
2021-07-07 20:22:40 +0400

    Add files via upload

---
+++ xndir 16.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+int main() {
+	int sum = 0;
+	int mul = 1;
+	int mid = 0;
+	int arr[] = { 10,15,20,25 };
+	for (int i = 0; i < 4; ++i) {
+		sum += arr[i];
+		mul *= arr[i];
+	}
+	mid = (sum + mul) / 2;
+	std::cout << sum << std::endl;
+	std::cout << mul << std::endl;
+	std::cout << mid;
+}
