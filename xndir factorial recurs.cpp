e650fee3aeb108accb25eff05d4fcbbfd4020f5d
Levon <73011326+Levon1999@users.noreply.github.com>
2021-07-08 20:07:49 +0400

    Add files via upload

---
+++ xndir factorial recurs.cpp
@@ -0,0 +1,12 @@
+﻿#include<iostream>
+int fact(int num) {
+	if (num == 0 || num == 1) {
+		return 1;
+	}
+	return num * fact(num - 1);
+}
+int main() {
+	int num = 0;
+	std::cin >> num;
+	std::cout << fact(num);
+}
