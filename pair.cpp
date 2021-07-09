0e4c74e124231e59d1c808b14437531963268534
Levon <73011326+Levon1999@users.noreply.github.com>
2021-07-10 00:36:28 +0400

    Add files via upload

---
+++ pair.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+#include<utility>
+#include<string>
+std::pair<std::string, double> foo(std::string str, double x) {
+	std::pair<std::string, double>Product1;
+	Product1 = std::make_pair(str, x);
+	std::cout << str << std::endl;
+	std::cout << x;
+	return Product1;
+}
+int main() {
+	std::cout << "Enter product name and price " << std::endl;
+	std::string x;
+	getline(std::cin, x);
+	double a = 0;
+	std::cin >> a;
+	foo(x, a);
+}
