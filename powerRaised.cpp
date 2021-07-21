22a073bf39daf72790df8abc84154971cd437853
Levon <73011326+Levon1999@users.noreply.github.com>
2021-07-21 20:56:56 +0400

    Add files via upload

---
+++ powerRaised.cpp
@@ -0,0 +1,10 @@
+﻿#include<iostream>
+int calculatePower(int base, int powerRaised) {
+	if (powerRaised < base) {
+		return base;
+	};
+	return base * calculatePower(base ,--powerRaised);
+}
+int main() {
+	std::cout << calculatePower(2, 5);
+}
