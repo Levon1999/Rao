5bc87230899d821e45c9aeae74bb410eb5ac4daf
Levon <73011326+Levon1999@users.noreply.github.com>
2021-07-23 21:29:54 +0400

    Add files via upload

---
+++ fibonacchi.cpp
@@ -0,0 +1,13 @@
+﻿#include<iostream>
+int main() {
+	int n = 0;
+	int fib = 0;
+	int t = 1;
+	std::cin >> n;
+	for (int i = 0; i < n; ++i) {
+		int sum = fib + t;
+		fib = t;
+		t = sum;
+	}
+	std::cout << t;
+}
