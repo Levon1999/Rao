4a63bfb3031b651dcec782af8d12c1a51e9e6880
Levon <73011326+Levon1999@users.noreply.github.com>
2021-07-25 15:09:03 +0400

    Add files via upload

---
+++ tox rekursiv.cpp
@@ -0,0 +1,12 @@
+﻿#include<iostream>
+int tmp = 0;
+int getlen(const char* str) {
+	if (*str == '\0') {
+		return 0;
+	}
+	return 1 + getlen(++str);
+}
+int main() {
+	const char* st = "germania";
+	std::cout << getlen("germania");
+}
