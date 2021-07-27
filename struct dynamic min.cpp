c4a435a9e650b423aa1a7095698d6956c194124b
Levon <73011326+Levon1999@users.noreply.github.com>
2021-07-27 15:38:40 +0400

    Add files via upload

---
+++ struct dynamic min.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+struct paper {
+	int length;
+	int high;
+};
+void rec(paper* arr, int n) {
+	static paper min = *arr;//static veragrum e mi angam amen recursiv kanchi yntacqum
+	if (!n) {
+		std::cout << min.high << std::endl;
+		return;
+	};
+	if (min.high > arr->high) {
+		min = *arr;
+	}
+	return rec(arr + 1, n - 1);
+}
+int main() {
+	int n = 0;
+	std::cin >> n;
+	paper* arr = new paper[n];
+	for (int i = 0; i < n; ++i) {
+		std::cin >> arr[i].length
+			>> arr[i].high;
+	};
+	rec(arr, n);
+	delete[]arr;
+	arr = nullptr;
+}
