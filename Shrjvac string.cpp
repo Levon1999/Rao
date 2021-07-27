7835f66b04bfdd46211c335fdd44b0daa1678272
Levon <73011326+Levon1999@users.noreply.github.com>
2021-07-27 15:39:06 +0400

    Add files via upload

---
+++ Shrjvac string.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+char* reverse(char* arr) {
+	int tmp = 0;
+	char* s = arr;
+	while (*s != '\0') {
+		++s;
+		++tmp;
+	}
+	for (int i = 0; i < tmp; ++i) {
+		char count = arr[i];
+		arr[i] = arr[tmp - 1];
+		arr[tmp - 1] = count;
+		--tmp;
+	}
+	return s;
+}
+int main() {
+	char arr[20] = { 0 };
+	std::cin >> arr;
+	reverse(arr);
+	std::cout << arr;
+}
