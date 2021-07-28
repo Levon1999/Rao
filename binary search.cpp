755559c0df10e2ca9d4d575ec40e4c8ebb8d3818
Levon <73011326+Levon1999@users.noreply.github.com>
2021-07-28 13:01:33 +0400

    Add files via upload

---
+++ binary search.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+int main() {
+	const int size = 5;
+	int arr[size] = { 2,4,6,8,11 };
+	int key = 0;
+	std::cin >> key;
+	int start = 0;
+	int end = size - 1;
+	while (start <= end) {
+		int mid = (start + end) / 2;
+		if (arr[mid] == key) {
+			std::cout << mid;
+			break;
+		}
+		if (arr[mid] < key) {
+			start = mid + 1;
+		}
+		else {
+			end = mid - 1;
+		}
+	}
+}
