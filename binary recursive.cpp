65c6d5d9f4278a2f7e9023fea4a310e50430fe81
Levon <73011326+Levon1999@users.noreply.github.com>
2021-07-28 12:41:22 +0400

    Add files via upload

---
+++ binary recursive.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+int binarySearch(int arr[], int start, int end, int key)
+{
+    if (start<=end) {
+        int mid = (start + end) / 2;
+        if (arr[mid] == key)
+            return mid;
+        if (arr[mid] > key)
+            return binarySearch(arr, start, mid - 1, key);
+        return binarySearch(arr, mid + 1, end, key);
+    }
+    return -1;
+}
+
+int main ()
+{
+    int arr[] = { 2, 3, 4, 10, 40 };
+    int key = 0;
+    cin >> key;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int result = binarySearch(arr, 0, n, key);
+    cout << "Element is not present in array " << result;
+    return 0;
+}
